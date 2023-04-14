f5263850ce3210d4d4e07737e8bf4e9ab2055004
David Onyemaenu <david.onyemaenu@gmail.com>
2023-04-13 17:24:27 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-''' function that returns the list 
-'''
+''' function that returns the list '''
 
 def lookup(obj):
-    '''function: lookup()
-    Reeturns a list object
-    '''
+    ''' function: lookup() Returns a list object '''
     return dir(obj)
-
